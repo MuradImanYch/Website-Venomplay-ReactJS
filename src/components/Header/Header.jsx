9f68dd2f5ebb80918187ef838b5e76e78a4e562d
Murad Imanov <mura.imanov@gmail.com>
2023-05-26 12:14:19 +0400

    header is done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './Header.css';
+import $ from 'jquery';
+
+import gh from '../../assets/ico/github.png';
+import twitter from '../../assets/ico/twitter.png';
+import dc from '../../assets/ico/dc.png';
+import tg from '../../assets/ico/tg.png';
+import en from '../../assets/ico/en.png';
+import ru from '../../assets/ico/ru.png';
+
+const Header = () => {
+    const[barState, setBarstate] = useState(true);
+
+    const menuToggle = () => {
+        if($(window).width() <= 1024) { // for mobile
+            setBarstate(!barState);
+            if(barState) {
+                $('.bar1').css({'transform': 'rotate(-45deg) translate(-8px, 7px)'});
+                $('.bar2').css({'opacity': '0'});
+                $('.bar3').css({'transform': 'rotate(45deg) translate(-8px, -8px)'});
+                $('header .listLangSwitcher').css({'transform': 'translate(0, 0)'});
+                $('body').css({'overflow': 'hidden'});
+                $('header .listLangSwitcher').css({'overflow':'scroll'});
+            }
+            else {
+                $('.bar1').css({'transform': 'rotate(0deg) translate(0px, 0px)'});
+                $('.bar2').css({'opacity': '1'});
+                $('.bar3').css({'transform': 'rotate(0deg) translate(0px, 0px)'});
+                $('header .listLangSwitcher').css({'transform': 'translate(0, -120%)'});
+                $('body').css({'overflow': 'auto'});
+                $('header .listLangSwitcher').css({'overflow':'hidden'});
+            }
+        }
+    }
+
+    return (
+        <div>
+            <header>
+                <div className="container">
+                    <div className='logoSocNet'>
+                        <div className='gap'></div>
+                        <div>
+                            <a className="logo" href="/">Logo</a>
+                        </div>
+                        <div className="toggle">
+                            <div id="menuToggleMobDiv" onClick={menuToggle}>
+                            <div className="bar1"></div>
+                            <div className="bar2"></div>
+                            <div className="bar3"></div>
+                        </div>
+                        </div>
+                        <div className="socNet">
+                            <a href='#'><img src={gh} alt="gh" /></a>
+                            <a href='#'><img src={twitter} alt="twitter" /></a>
+                            <a href='#'><img src={dc} alt="dc" /></a>
+                            <a href='#'><img src={tg} alt="tg" /></a>
+                        </div>
+                    </div>
+                    <div className='listLangSwitcher'>
+                        <ul>
+                            <li><a href="#">Play</a></li>
+                            <li><a href="#">Mint</a></li>
+                            <li><a href="#">Stake</a></li>
+                            <li><a href="#">Swap</a></li>
+                            <li><a href="#">Farm</a></li>
+                            <li><a href="#">White paper</a></li>
+                            <li><a href="#">F.A.Q</a></li>
+                        </ul>
+                        <div className="socNet">
+                            <a href='#'><img src={gh} alt="gh" /></a>
+                            <a href='#'><img src={twitter} alt="twitter" /></a>
+                            <a href='#'><img src={dc} alt="dc" /></a>
+                            <a href='#'><img src={tg} alt="tg" /></a>
+                        </div>
+                        <div className="langSwitcher">
+                            <a href="#"><img src={en} alt="en" /></a>
+                            <a href="#"><img src={ru} alt="ru" /></a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
